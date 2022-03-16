d0234ce05aa4a7b7baa4f89f3451e9c18ba03585
trunghq3101 <trunghq3101@gmail.com>
2022-03-16 20:52:16 +0700

    Use FutureBuilder to readTodoList

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:architecture_bloc_sample/data/models/todo.model.dart';
+import 'package:architecture_bloc_sample/data/todo_storage.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,24 +7,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final todoList = todoStorage.readTodoList();
     return Scaffold(
       backgroundColor: Colors.amber,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: todoList
-                .map(
-                  (e) => Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Text(e.title),
-                    ),
-                  ),
-                )
-                .toList(),
-          ),
+          child: FutureBuilder<List<Todo>>(
+              future: todoStorage.readTodoList(),
+              builder: (_, snapshot) {
+                if (snapshot.hasData) {
+                  final todoList = snapshot.data!;
+                  return ListView(
+                    children: todoList
+                        .map(
+                          (e) => Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(24),
+                              child: Text(e.title),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text("Something went wrong"),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
         ),
       ),
     );
