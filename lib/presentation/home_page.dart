4b344f433c5119261ec041fa566e8eece7adb96d
trunghq3101 <trunghq3101@gmail.com>
2022-03-16 18:23:56 +0700

    Use mock todoStorage

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,17 +1,11 @@
-import 'package:architecture_bloc_sample/data/todo.model.dart';
 import 'package:flutter/material.dart';
-
-final todoList = [
-  Todo(title: 'Do something 1'),
-  Todo(title: 'Do something 2'),
-  Todo(title: 'Do something 3'),
-];
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final todoList = todoStorage.readTodoList();
     return Scaffold(
       backgroundColor: Colors.amber,
       body: SafeArea(
