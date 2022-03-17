69af1517cce1d3bdec8d9929153d95484978e3fb
trunghq3101 <trunghq3101@gmail.com>
2022-03-17 10:32:52 +0700

    Random title from predefined list

--- lib/business/todo_bloc.dart
+++ lib/business/todo_bloc.dart
@@ -3,6 +3,19 @@ import 'dart:math';
 import 'package:architecture_bloc_sample/data/models/todo.model.dart';
 import 'package:architecture_bloc_sample/data/todo_storage.dart';
 
+const kTodoTitleList = [
+  'Be yourself; everyone else is already taken.',
+  '''Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.''',
+  'So many books, so little time.',
+  'A room without books is like a body without a soul.',
+  'You only live once, but if you do it right, once is enough.',
+  'Be the change that you wish to see in the world.',
+  '''In three words I can sum up everything I've learned about life: it goes on.''',
+  'A friend is someone who knows all about you and still loves you.',
+  'To live is the rarest thing in the world. Most people exist, that is all.',
+  'Always forgive your enemies; nothing annoys them so much.'
+];
+
 final TodoBloc todoBloc = TodoBloc(todoStorage: todoStorage);
 
 class TodoBloc {
@@ -18,11 +31,6 @@ class TodoBloc {
 
   String _getRandomTitle() {
     final r = Random();
-    return String.fromCharCodes(
-      List.generate(
-        50,
-        (index) => r.nextInt(33) + 89,
-      ),
-    );
+    return kTodoTitleList[r.nextInt(10)];
   }
 }
